9323f7f5c3b2cdd2f36bd3dcb1ee7a091d674596
hatice <haticeesurumlu@gmail.com>
2023-06-26 13:47:40 +0300

    feat: add the cart

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: "Socks",
-      image: "./assets/images/socks_green.jpg",
+      image: "./assets/images/socks_blue.jpg",
       inStock: true,
-      inventory: 6,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green" },
-        { id: 2235, color: "blue" },
+        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
       ],
-      sizes: ["S", "M", "L", "XL"],
     };
   },
 });
