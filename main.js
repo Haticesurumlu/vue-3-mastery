a7157728f8a620d695ad0906fed8caada71ab12b
hatice <haticeesurumlu@gmail.com>
2023-06-26 13:45:45 +0300

    feat : add detail  and variant

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const app = Vue.createApp({
       image: "./assets/images/socks_green.jpg",
       inStock: true,
       inventory: 6,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
+      sizes: ["S", "M", "L", "XL"],
     };
   },
 });
