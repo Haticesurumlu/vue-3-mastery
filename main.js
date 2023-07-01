ef522802ac6c109ca3cb62c79f597502a8552013
hatice <haticeesurumlu@gmail.com>
2023-07-02 01:38:03 +0300

    fix: change the url

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: "Socks",
+      brand: "Vue Mastery",
       image: "./assets/images/socks_blue.jpg",
-      inStock: true,
+      inStock: false,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
         { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
@@ -12,9 +13,8 @@ const app = Vue.createApp({
       ],
     };
   },
-
   methods: {
-    addToCard() {
+    addToCart() {
       this.cart += 1;
     },
     updateImage(variantImage) {
