57a9bf8385d6e3fafc4744452658c7eda4daf60c
hatice <haticeesurumlu@gmail.com>
2023-07-03 11:07:51 +0300

    class & style binding

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: "Socks",
-      brand: "Vue Mastery",
+      brand: "Hatice",
       image: "./assets/images/socks_blue.jpg",
-      inStock: false,
+      inStock: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
         { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
@@ -19,6 +19,11 @@ const app = Vue.createApp({
     },
     updateImage(variantImage) {
       this.image = variantImage;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
     },
   },
 });
