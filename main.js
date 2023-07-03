33e002d78a390b1789bd0e1621359ea792426c50
hatice <haticeesurumlu@gmail.com>
2023-07-03 14:06:57 +0300

    feat: add quantity

--- main.js
+++ main.js
@@ -4,12 +4,23 @@ const app = Vue.createApp({
       cart: 0,
       product: "Socks",
       brand: "Vue Mastery",
+
       image: "./assets/images/socks_blue.jpg",
       inStock: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
       ],
     };
   },
@@ -17,6 +28,7 @@ const app = Vue.createApp({
     addToCart() {
       this.cart += 1;
     },
+
     updateImage(variantImage) {
       this.image = variantImage;
     },
